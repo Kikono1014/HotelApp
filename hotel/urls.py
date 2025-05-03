4da14566d52bc8879504af8f01039e3d55b5ada6
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2025-05-03 14:42:18 +0300

    added basic views and templates

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from .views import RoomListView, RoomDetailView
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rooms/', RoomListView.as_view(), name='list'),
+    path('rooms/<int:pk>/', RoomDetailView.as_view(), name='detail'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Room
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'HotelA',
+        'description': 'Booking rooms in HotelA.'
+    }
+    return render(request, 'index.html', context)
+
+
+class RoomListView(ListView):
+    model = Room
+    template_name = 'rooms/index.html'
+    context_object_name = 'rooms'
+    paginate_by = 10
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'rooms/detail.html'
+    context_object_name = 'room'
