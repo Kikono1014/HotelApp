746b4188450f74141d41606e3ca95db532a7822b
Artem Hryshchenko <artemgrisenko039@gmail.com>
2025-05-06 11:00:09 +0300

    Add path for Booking

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import RoomListView, RoomDetailView
+from .views import RoomListView, RoomDetailView, BookingCreateView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('rooms/', RoomListView.as_view(), name='list'),
     path('rooms/<int:pk>/', RoomDetailView.as_view(), name='detail'),
+    path('book/<int:pk>/', BookingCreateView.as_view(), name='book'),  # Новий маршрут для бронювання
 ]
