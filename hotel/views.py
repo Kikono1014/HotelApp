819cdf5036f0d52815de14264e19c24bcd892cbe
Artem Hryshchenko <artemgrisenko039@gmail.com>
2025-05-06 10:58:41 +0300

    Add BookingCreateView

--- hotel/views.py
+++ hotel/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Room
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import Room, Guest, Booking
 from .filters import RoomFilter
-from .forms import RoomFilterForm
+from .forms import RoomFilterForm, BookingForm
 
 def index(request):
     context = {
@@ -10,7 +11,6 @@ def index(request):
         'description': 'Booking rooms in HotelA.'
     }
     return render(request, 'index.html', context)
-
 
 class RoomListView(ListView):
     model = Room
@@ -20,23 +20,57 @@ class RoomListView(ListView):
 
     filterset_class = RoomFilter
 
-   
     def get_queryset(self):
         qs = super().get_queryset()
-
         data = self.request.GET.copy()
-
         self.filterset = self.filterset_class(data, queryset=qs)
-
         return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter_form'] = RoomFilterForm(self.request.GET)
-        context['filterset']   = self.filterset
+        context['filterset'] = self.filterset
         return context
 
 class RoomDetailView(DetailView):
     model = Room
     template_name = 'rooms/detail.html'
     context_object_name = 'room'
+
+class BookingCreateView(CreateView):
+    form_class = BookingForm
+    template_name = 'book.html'
+    success_url = reverse_lazy('list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['room'] = Room.objects.get(pk=self.kwargs['pk'])
+        return context
+
+    def form_valid(self, form):
+        # Отримуємо номер
+        room = Room.objects.get(pk=self.kwargs['pk'])
+
+        # Створюємо або отримуємо гостя
+        guest, created = Guest.objects.get_or_create(
+            email=form.cleaned_data['email'],
+            defaults={
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'phone': form.cleaned_data['phone'] or None
+            }
+        )
+
+        # Створюємо бронювання
+        booking = form.save(commit=False)  # Не зберігаємо одразу
+        booking.room = room
+        booking.guest = guest
+        booking.status = 'confirmed'
+        booking.booking_channel = 'online'
+        booking.save()
+
+        # Оновлюємо статус номера
+        room.is_available = False
+        room.save()
+
+        return super().form_valid(form)
