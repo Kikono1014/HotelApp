58ff76042d728478a74ea33427a540163af81c11
Andrii Volkov <qwerttyuiop050@ukr.net>
2025-05-22 23:25:38 +0300

    create structure for views

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 from . import views
-from .views import RoomListView, RoomDetailView, BookingCreateView
+from .views import (
+    index,
+    RoomListView,
+    RoomDetailView,
+    BookingCreateView,
+    BookingListView,
+    BookingDetailView,
+    BookingCancelView,
+    GuestBookingListView,
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
     path('rooms/', RoomListView.as_view(), name='list'),
     path('rooms/<int:pk>/', RoomDetailView.as_view(), name='detail'),
-    path('book/<int:pk>/', BookingCreateView.as_view(), name='book'),  # Новий маршрут для бронювання
+    path('rooms/<int:pk>/book/', BookingCreateView.as_view(), name='book'),
+
+    # Новые маршруты для бронирований
+    path('bookings/', BookingListView.as_view(), name='booking_list'),
+    path('bookings/<int:pk>/', BookingDetailView.as_view(), name='booking_detail'),
+    path('bookings/<int:pk>/cancel/', BookingCancelView.as_view(), name='booking_cancel'),
+    path('my-bookings/', GuestBookingListView.as_view(), name='guest_booking_list'),
 ]
